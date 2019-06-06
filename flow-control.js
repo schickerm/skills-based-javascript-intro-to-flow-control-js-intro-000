cf061b6ab952284493321ac931985f413ac001d4
Melanie Schicker <melanie.schicker@gmail.com>
2019-06-06 16:17:04 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -5,18 +5,18 @@ function basicTeenager(age) {
 
 function teenager(age) {
  if (age >=13 && age <=19) 
-  {return "You are a teenager!";} 
+  {return "You are a teenager!"} 
  else 
-  {return "You are not a teenager";}
+  {return "You are not a teenager"}
 }
 
 function ageChecker(age) {
  if (age >=13 && age <=19) 
-  {return "You are a teenager!";} 
+  {return "You are a teenager!"} 
  else if (age <=12) 
-  {return "You are a kid";} 
+  {return "You are a kid"} 
  else
-  {return "You are a grownup";}
+  {return "You are a grownup"}
 }
 
 function ternaryTeenager(age) {
